61cc2164007fe60b8d549b3a54d190b71c5c0bd9
NuvoloniM <nuvoloni.m@gmail.com>
2022-03-10 17:07:19 +0100

    funzione per calcolare prezzo

--- asset/app.js
+++ asset/app.js
@@ -6,15 +6,41 @@ let ageInput = document.getElementById('age');
 // variabile per selezionare il bottone submit
 let submitBtn = document.getElementById('button');
 
+//seleziono il div del biglietto
+let biglietto = document.getElementById('biglietto');
+
+// calcolo il prezzo base del biglietto dati i chilometri
+let tax = 0.21;
+let total = tax * distanceInput.value;
+
+
+//creo evento ' al click calcolo prezzo'
+submitBtn.addEventListener('click', calcPrice);
+
 //creo evento 'click bottone submit'
 submitBtn.addEventListener('click', saveData);
 
 
 
-//creo funzione
-
+//creo funzione per stampare il biglietto coi dati
 function saveData() {
-    console.log(distanceInput.value);
-    console.log(nameInput.value);
-    console.log(ageInput.value);
+   biglietto.innerHTML += `<p> ${nameInput.value} </p> <p> ${distanceInput.value} </p>`;
+}
+
+//creo funzione per calcolare il prezzo del biglietto
+function calcPrice() {
+    if (ageInput.value == 'minorenne') {
+        total = total * 0.8;
+        total = total.toFixed(2);
+        biglietto.innerHTML += `il prezzo del tuo biglietto è ${total} &euro;`;
+    } else if (ageInput.value == 'over65'){
+        total = total * 0.6;
+        total = total.toFixed(2);
+        biglietto.innerHTML += `il prezzo del tuo biglietto è ${total} &euro;`;
+    } else {
+        total = total;
+        total = total.toFixed(2);
+        biglietto.innerHTML += `il prezzo del tuo biglietto è ${total} &euro;`;
+    }
+    
 }
