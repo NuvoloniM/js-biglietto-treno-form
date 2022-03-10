6b7b6c1fa94bbb01ccbd3f4649369ef4e3944644
NuvoloniM <nuvoloni.m@gmail.com>
2022-03-10 19:01:02 +0100

    event to create/delete ticket

--- asset/app.js
+++ asset/app.js
@@ -6,6 +6,9 @@ let ageInput = document.getElementById('age');
 // variabile per selezionare il bottone submit
 let submitBtn = document.getElementById('button');
 
+//variabile per selezionare il bottone annulla
+let delBtn = document.getElementById('remove');
+
 //seleziono i div del biglietto
 //nome passeggero
 let passenger = document.getElementById('passenger');
@@ -21,6 +24,7 @@ let price = document.getElementById('price');
 // calcolo il prezzo base del biglietto dati i chilometri
 let tax = 0.21;
 let total = tax * distanceInput.value;
+let promotion = "Biglietto Standard";
 
 //numeri casuali per la carrozza del treno (tra 1 e 9)
 let randCab = Math.floor(Math.random()*9) + 1;
@@ -28,35 +32,55 @@ let randCab = Math.floor(Math.random()*9) + 1;
 //numeri casuali per il Codice CP
 let randCP = Math.floor(Math.random()*99999) + 1;
 
+//selezione il biglietto
+let ticket = document.getElementById('ticket');
+
 //creo evento ' al click calcolo prezzo'
 submitBtn.addEventListener('click', calcPrice);
 
-//creo evento 'click bottone submit'
+//creo evento 'click bottone stampo dati su biglietto'
 submitBtn.addEventListener('click', saveData);
 
+//stampo biglietto
+submitBtn.addEventListener('click', stamp);
+
+//elimino il biglietto
+delBtn.addEventListener('click', del);
 
 
-//creo funzione per stampare il biglietto coi dati
-function saveData() {
-    passenger.innerHTML = `${nameInput.value}`;
-    cab.innerHTML = `${randCab}`;
-    code.innerHTML = `${randCP}`
-}
 
 //creo funzione per calcolare il prezzo del biglietto
 function calcPrice() {
     if (ageInput.value == 'minorenne') {
+        promotion = "Biglietto Young";
         total = total * 0.8;
         total = total.toFixed(2);
-        biglietto.innerHTML += `il prezzo del tuo biglietto è ${total} &euro;`;
     } else if (ageInput.value == 'over65'){
+        promotion = "Biglietto Over";
         total = total * 0.6;
         total = total.toFixed(2);
-        biglietto.innerHTML += `il prezzo del tuo biglietto è ${total} &euro;`;
     } else {
         total = total;
         total = total.toFixed(2);
-        biglietto.innerHTML += `il prezzo del tuo biglietto è ${total} &euro;`;
     }
     
+}
+
+//creo funzione per stampare il biglietto coi dati
+function saveData() {
+    passenger.innerHTML = `${nameInput.value}`;
+    promo.innerHTML = `${promotion}`;
+    cab.innerHTML = `${randCab}`;
+    code.innerHTML = `${randCP}`;
+    price.innerHTML = `${total} &euro;`;
+}
+
+//funzione per stampare biglietto
+function stamp() {
+    ticket.className = "d_block";
+}
+
+//funzione per eliminare il biglietto 
+function del() {
+    ticket.className = "display";
 }
